6e172dd57c89a3b51d6ec0e2dc6b8c5775683681
Krishna Ghadge <krushnaghadge01@gmail.com>
2025-09-27 10:23:48 +0530

    working on the  Api started

--- app/Http/Controllers/Front/AccountController.php
+++ app/Http/Controllers/Front/AccountController.php
@@ -8,4 +8,30 @@ use Illuminate\Http\Request;
 class AccountController extends Controller
 {
     //
+    public function register(Request $request){
+        $validator =  Validator::make($request->all(),[
+            'name'=>'required|string|max:255',
+            'email'=>'required|email|unique:users,email',
+            //'password'=>'required|min:6|confirmed',
+        ]);
+
+
+if($validator->fails()){
+            return response()->json([
+                'status'=>400,
+                'errors'=>$validator->errors(),
+            ]);
+        }
+
+//now save user data
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return response()->json([
+            'status'=>200,
+            'message'=>'User registered successfully',
+        ]);
+    }
 }
